b8dcc97a79563726e8e121f3ca07a4f30be66f11
wuyepabdul <wuyepabdul@gmail.com>
2023-12-20 01:46:12 +0100

    Add vercel frontend link

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: true,
+  origin: ["https://challenge-mern-frontend.vercel.app/"],
+  methods: ["POST", "GET", "PUT"],
+  credentials: true,
 };
 
 app.get("/api", (req, res) => {
